e01ec4222dcef0b09941f81e4f8b12cc0beaf687
Victor Alfaro <victor.alfaro@dotcms.com>
2021-08-21 20:43:01 -0600

    docker build test

--- docker/semefa-gateway/build-src/build_semefa_gateway.sh
+++ docker/semefa-gateway/build-src/build_semefa_gateway.sh
@@ -11,12 +11,12 @@ function buildByCommit {
   git pull
 
   echo "Checking out commit/tag/branch: $1"
-  if [[ "${branch}" != 'master' ]]; then
+  if [[ "${branch}" != 'master' && "${branch}" != 'main' ]]; then
     echo "Executing: checkout -b ${branch} --track origin/${branch}"
     git checkout -b ${branch} --track origin/${branch}
   fi
 
-  ./gradlew clean install -x test
+  ./gradlew clean build -x test
   ./gradlew bootJar --no-daemon --build-cache
   cp ${src_folder}/build/libs/semefa-gateway-1.0.jar /app
   cp ${src_folder}/application.yml /app
--- docker/semefa-gateway/entrypoint.sh
+++ docker/semefa-gateway/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -jar ./semefa-gateway-1.0.jar --spring.config.location=file:./application.yml
+
+/bin/bash
