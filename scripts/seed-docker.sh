8096adc56e39d8c5025f5c48375afadf2d50c9f4
Victor Alfaro <victor.alfaro@dotcms.com>
2021-08-24 09:11:58 -0600

    docker image build test

--- scripts/seed-docker.sh
+++ scripts/seed-docker.sh
@@ -6,11 +6,10 @@
 script_resources=(docker-compose.yml runGw.sh setup.sh)
 target_dir=${HOME}/.sac/semefa-gateway
 
+[[ -d ${target_dir} ]] && rm -rf ${target_dir}
 mkdir -p ${target_dir}
-cp *.env.* ${target_dir}
+mv ./*.env.* ${target_dir}
 cd ${target_dir}
-
-[[ -d semefa-gateway ]] && rm -rf ./semefa-gateway
 git clone https://github.com/vicozizou/semefa-gateway.git
 ls -las .
 
