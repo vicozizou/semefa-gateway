ecaf1002886b618a7a93ec0dfdbc2b21d94dd609
Victor Alfaro <victor.alfaro@dotcms.com>
2021-08-23 23:49:35 -0600

    docker image build test

--- scripts/runGw.sh
+++ scripts/runGw.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 profile=${1}
+echo "Specified profile: ${profile}"
 [[ -z "${profile}" ]] && profile='dev'
 env_file=".env.${profile}"
+echo "Discovered env file: ${profile}"
 
 [[ ! -f ${env_file} ]] && echo "No existe archivo .env.<perfil>, abortando"
-env $(cat ${env_file} | xargs) docker-compose -f docker-compose.yml
+env $(cat ${env_file} | xargs) docker-compose -f docker-compose.yml up
